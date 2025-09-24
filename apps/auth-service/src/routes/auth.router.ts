585a6a34e1661fbe1fdfce852f131d68ae944ce5
Kartik Verma <kartikverma90363@gmail.com>
2025-09-24 05:45:29 +0530

    forgot password

--- apps/auth-service/src/routes/auth.router.ts
+++ apps/auth-service/src/routes/auth.router.ts
@@ -1,5 +1,6 @@
 import express, { Router } from "express";
-import { loginUser, resetUserPassword, userForgotPassword, UserRegistration, verifyUser, verifyUserForgotPassword,  } from "../controller/auth.controller";
+import { loginUser, resetUserPassword, userForgotPassword, UserRegistration, verifyUser } from "../controller/auth.controller";
+import { verifyForgotPasswordOtp } from "../utils/auth.helper";
 
 const router: Router = express.Router();
 
@@ -7,7 +8,7 @@ router.post("/user-registration", UserRegistration);
 router.post("/verify-user", verifyUser);
 router.post("/login-user", loginUser);
 router.post("/forgot-password-user", userForgotPassword);
-router.post("/reset-user-password", resetUserPassword);
-router.post("/verify-forgot-password-user", verifyUserForgotPassword);
+router.post("/reset-password-user", resetUserPassword);
+router.post("/verify-forgot-password-user", verifyForgotPasswordOtp);
 
 export default router;
--- apps/auth-service/src/utils/auth.helper.ts
+++ apps/auth-service/src/utils/auth.helper.ts
@@ -1,85 +1,3 @@
-// import { ValidationError } from "./../../../../packages/error-handler/index";
-// import crypto from "crypto";
-// import redis from "../../../../packages/libs/redis";
-// import { sendEmail } from "./sendMail";
-// import { NextFunction } from "express";
-
-// const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-// export const validateRegistrationData = (
-//   data: any,
-//   userType: "user" | "seller"
-// ) => {
-//   const { name, email, password, phone_number, country } = data;
-
-//   // Check required fields
-//   if (
-//     !name ||
-//     !email ||
-//     !password ||
-//     (userType === "seller" && (!phone_number || !country))
-//   ) {
-//     throw new ValidationError("Missing required fields");
-//   }
-
-//   // Validate email format
-//   if (!emailRegex.test(email)) {
-//     throw new ValidationError("Invalid email format!");
-//   }
-// };
-
-// export const checkOtpRestriction = async (
-//   email: string,
-//   next: NextFunction
-// ) => {
-//   if (await redis.set(`otp_lock:${email}`)) {
-//     return next(
-//       new ValidationError(
-//         "Account locked due to multiple failed attempts, Try Again after 30minutes!!"
-//       )
-//     );
-//   }
-//   if (await redis.set(`otp_spam_lock:${email}`)) {
-//     return next(
-//       new ValidationError(
-//         "Too many OTP requests! Please wait 1hour before requesting again"
-//       )
-//     );
-//   }
-
-//   if (await redis.set(`otp_cooldown:${email}`)) {
-//     return next(
-//       new ValidationError("Please wait 1 min before requesting a new OTP!")
-//     );
-//   }
-// };
-
-// export const trackOtpRequests = async (email: string, next: NextFunction) => {
-//   const otpRequestKey = `otp_request_count:${email}`;
-//   let otpRequests = parseInt((await redis.set(otpRequestKey)) || "0");
-
-//   if (otpRequests >= 2) {
-//     await redis.set(`otp_spam_lock:${email}`, "locked", "EX", 3600);
-//     return next(
-//       new ValidationError(
-//         "Too many OTP requests .Wait for 1hour to request again."
-//       )
-//     );
-//   }
-//   await redis.set(otpRequestKey, otpRequests + 1, "EX", 3600);
-// };
-
-// export const sendOtp = async (
-//   name: string,
-//   email: string,
-//   template: string
-// ) => {
-//   const otp = crypto.randomInt(1000, 9999).toString();
-//   await sendEmail(email, "Verify your email", template, { name, otp });
-//   await redis.set(`otp:${email}`, otp, "EX", 300);
-//   await redis.set(`otp_cooldown:${email}`, "true", "EX", 60);
-// };
-
 import { ValidationError } from "@packages/error-handler";
 import crypto from "crypto";
 import redis from "../../../../packages/libs/redis";
@@ -195,36 +113,6 @@ export const verifyOtp = async (
   await redis.del(`otp:${email}`, failedAttemptsKey);
 };
 
-// export const handleForgotPassword = async (
-//   req: Request,
-//   res: Response,
-//   next: NextFunction,
-//   userType: "user" | "seller"
-// ) => {
-//   try {
-//     const { email } = req.body as { email?: string };
-
-//     if (!email) throw new ValidationError("Email is required!");
-
-//     const user =
-//       userType === "user" &&
-//       (await prisma.users.findUnique({ where: { email } }));
-
-//     if (!user) throw new ValidationError(`${userType} not found!`);
-
-//     await checkOtpRestriction(email, next);
-//     await trackOtpRequests(email, next);
-
-//     await sendOtp(email, user.name, "forgot-password-user-mail");
-
-//     res
-//       .status(200)
-//       .json({ message: "OTP sent to email. Please verify your account" });
-//   } catch (error) {
-//     next(error);
-//   }
-// };
-
 export const handleForgotPassword = async (
   req: Request,
   res: Response,
@@ -232,21 +120,20 @@ export const handleForgotPassword = async (
   userType: "user" | "seller"
 ) => {
   try {
-    const { email } = req.body as { email?: string };
+    const { email } = req.body;
 
     if (!email) throw new ValidationError("Email is required!");
 
-    const user = userType === "user";
-    await prisma.users.findUnique({ where: { email } });
+    const user =
+      userType === "user" &&
+      (await prisma.users.findUnique({ where: { email } }));
 
     if (!user) throw new ValidationError(`${userType} not found!`);
 
     await checkOtpRestriction(email, next);
     await trackOtpRequests(email, next);
 
-    await sendOtp(user.name, email, "forgot-password-user-mail");
-
-    console.log("Sent forgot-password OTP to:", email);
+    await sendOtp(user.name,email, "forgot-password-user-mail");
 
     res
       .status(200)
@@ -259,8 +146,8 @@ export const handleForgotPassword = async (
 export const verifyForgotPasswordOtp = async (
   req: Request,
   res: Response,
-  next: NextFunction
-): Promise<void> => {
+  next: NextFunction,
+) => {
   try {
     const { email, otp } = req.body;
 
